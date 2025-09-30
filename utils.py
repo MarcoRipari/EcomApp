35b970171b55d85b4e8ad448f11ac680d7b36509
MarcoRipari <marco.ripari.falc@gmail.com>
2025-09-30 18:29:01 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 import os
 import importlib
 
-def load_functions_from(folder_name, global_scope):
-    import os
-    import importlib
-
+def load_functions_from(folder_name):
+    """
+    Carica dinamicamente tutte le funzioni da una cartella
+    e le aggiunge allo scope globale del modulo che lo chiama.
+    """
     base_path = os.path.dirname(os.path.abspath(__file__))
-    folder_path = os.path.join(base_path, "..", folder_name)  # se utils.py
+    folder_path = os.path.join(base_path, folder_name)
 
     if not os.path.exists(folder_path):
         raise FileNotFoundError(f"La cartella {folder_path} non esiste!")
@@ -15,5 +16,4 @@ def load_functions_from(folder_name, global_scope):
         if file.endswith(".py") and file != "__init__.py":
             module_name = file[:-3]
             module = importlib.import_module(f"{folder_name}.{module_name}")
-            # aggiorna lo scope passato come argomento
-            global_scope.update({k: v for k, v in module.__dict__.items() if callable(v)})
+            globals().update({k: v for k, v in module.__dict__.items() if callable(v)})
