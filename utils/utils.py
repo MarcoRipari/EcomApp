4eb6ebc836901c66a8d3ade27dd176c74d172795
MarcoRipari <marco.ripari.falc@gmail.com>
2025-09-30 18:15:48 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,6 @@
-
+def load_functions_from(folder):
+    for file in os.listdir(folder):
+        if file.endswith(".py") and file != "__init__.py":
+            module_name = file[:-3]
+            module = importlib.import_module(f"{folder}.{module_name}")
+            globals().update({k: v for k, v in module.__dict__.items() if callable(v)})
