cb7fb9c34f180c834cc4d7883a9e58d308e8dfe8
MarcoRipari <marco.ripari.falc@gmail.com>
2025-09-30 18:07:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import streamlit as st
-from views import *
-from functions import *
+import os
+import importlib
+
+def load_functions_from(folder):
+    for file in os.listdir(folder):
+        if file.endswith(".py") and file != "__init__.py":
+            module_name = file[:-3]
+            module = importlib.import_module(f"{folder}.{module_name}")
+            globals().update({k: v for k, v in module.__dict__.items() if callable(v)})
+
+# Carica viste e funzioni
+load_functions_from("funzioni")
+load_functions_from("viste")
 
 st.set_page_config(page_title="Gestione ECOM", layout="wide")
 
