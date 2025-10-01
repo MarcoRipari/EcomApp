37c43a31a05ed8f2a28de26f2a68ba6dcdb6eab6
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-01 08:25:16 +0200

    Update foto.py

--- views/foto.py
+++ views/foto.py
@@ -9,5 +9,5 @@ def foto_import_ordini():
   uploaded_files = st.file_uploader("Carica i file CSV", type="csv", accept_multiple_files=True)
 
   if uploaded_files:
-    for file in uploaded_file:
+    for file in uploaded_files:
       st.write(file)
