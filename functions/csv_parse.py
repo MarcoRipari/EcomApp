04212488157cb8f97a5e72cfb38a28d6cd2c9085
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-01 08:22:57 +0200

    Update csv_parse.py

--- functions/csv_parse.py
+++ functions/csv_parse.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
 
+def read_csv_auto_encoding(uploaded_file, separatore=None):
+    raw_data = uploaded_file.read()
+    result = chardet.detect(raw_data)
+    encoding = result['encoding'] or 'utf-8'
+    uploaded_file.seek(0)  # Rewind after read
+    if separatore:
+        return pd.read_csv(uploaded_file, sep=separatore, encoding=encoding, dtype=str)
+    else:
+        return pd.read_csv(uploaded_file, encoding=encoding, dtype=str)
