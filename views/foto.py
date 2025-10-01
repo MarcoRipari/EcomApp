4e2621ad97b1f13485788fdae44ce9a8b1721def
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-01 11:43:00 +0200

    Update foto.py

--- views/foto.py
+++ views/foto.py
@@ -19,17 +19,22 @@ def foto_import_ordini():
   st.title("Importa ordini nuova stagione")
 
   uploaded_files = st.file_uploader("Carica i file CSV", type="csv", accept_multiple_files=True)
-
+  
+  df_totale = pd.DataFrame()
+  st.write(len(df_totale))
+  
   if uploaded_files:
     for file in uploaded_files:
       output = read_csv_auto_encoding(file)
       df = pd.DataFrame(output[1:])
       df["COD.CLIENTI"] = df["COD.CLIENTI"].map(map_cod_cli)
-
-      st.write(df)
-      
-      data = df.fillna("").astype(str)
-      data = data.values.tolist()
-      if st.button("Carica su GSheet"):
-        sheet_ordini.append_rows(data, value_input_option="RAW")
-        st.success("Caricati correttamente")
+      if len(df_totale)<=0:
+        df_totale = pd.DataFrame(df, columns=headers)
+      else:
+        df_totale.append(df)
+        
+    data = df_totale.fillna("").astype(str)
+    data = data.values.tolist()
+    if st.button("Carica su GSheet"):
+      sheet_ordini.append_rows(data, value_input_option="RAW")
+      st.success("Caricati correttamente")
