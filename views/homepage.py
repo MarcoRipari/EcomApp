47090d3dca9fc2c1f114aa064c2cf53a5860c6cd
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-01 07:03:12 +0200

    Update homepage.py

--- views/homepage.py
+++ views/homepage.py
@@ -10,6 +10,8 @@ def load_functions_from(folder_name):
     base_path = os.path.dirname(os.path.abspath(__file__))
     folder_path = os.path.join(base_path, folder_name)
 
+    st.write(folder_path)
+
     if not os.path.exists(folder_path):
         raise FileNotFoundError(f"La cartella {folder_path} non esiste!")
 
