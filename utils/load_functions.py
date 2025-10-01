cfa7b6d2f6c558cc3ad8325185c4ef67fba5752b
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-01 07:08:18 +0200

    Create load_functions.py

---
+++ utils/load_functions.py
@@ -0,0 +1,23 @@
+import os
+import importlib
+
+def load_functions_from(folder_name):
+    """
+    Carica dinamicamente tutte le funzioni da una cartella
+    e le aggiunge allo scope globale del modulo che lo chiama.
+    """
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    base_path = os.path.dirname(base_path)
+    #folder_path = os.path.join(base_path, folder_name)
+    folder_path = os.path.join(base_path, folder_name)
+
+    st.write(folder_path)
+
+    if not os.path.exists(folder_path):
+        raise FileNotFoundError(f"La cartella {folder_path} non esiste!")
+
+    for file in os.listdir(folder_path):
+        if file.endswith(".py") and file != "__init__.py":
+            module_name = file[:-3]
+            module = importlib.import_module(f"{folder_name}.{module_name}")
+            globals().update({k: v for k, v in module.__dict__.items() if callable(v)})
