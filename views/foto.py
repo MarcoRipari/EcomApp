19fcf194e18378721a334394e91f2d8636fa48e9
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-01 08:24:16 +0200

    Update foto.py

--- views/foto.py
+++ views/foto.py
@@ -6,4 +6,4 @@ load_functions_from("functions", globals())
 def foto_import_ordini():
   st.title("Importa ordini nuova stagione")
 
-  uploaded_files = st.upload_file("Carica i file CSV", type="csv", accept_multiple_files=True)
+  uploaded_files = st.file_uploader("Carica i file CSV", type="csv", accept_multiple_files=True)
