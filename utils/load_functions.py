7b0549f7b06aabcce52faf08d1ca775c76a7cef6
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-01 07:16:18 +0200

    Update load_functions.py

--- utils/load_functions.py
+++ utils/load_functions.py
@@ -2,14 +2,9 @@ import streamlit as st
 import os
 import importlib
 
-def load_functions_from(folder_name):
-    """
-    Carica dinamicamente tutte le funzioni da una cartella
-    e le aggiunge allo scope globale del modulo che lo chiama.
-    """
+def load_functions_from(folder_name, global_scope=None):
     base_path = os.path.dirname(os.path.abspath(__file__))
-    base_path = os.path.dirname(base_path)
-    #folder_path = os.path.join(base_path, folder_name)
+    base_path = os.path.dirname(base_path)  # vai a /ecomapp
     folder_path = os.path.join(base_path, folder_name)
 
     if not os.path.exists(folder_path):
@@ -19,4 +14,6 @@ def load_functions_from(folder_name):
         if file.endswith(".py") and file != "__init__.py":
             module_name = file[:-3]
             module = importlib.import_module(f"{folder_name}.{module_name}")
-            globals().update({k: v for k, v in module.__dict__.items() if callable(v)})
+            
+            target_scope = global_scope if global_scope is not None else globals()
+            target_scope.update({k: v for k, v in module.__dict__.items() if callable(v)})
