41deb8031eec60d193646a23f65de15181722862
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-02 07:36:27 +0200

    Update foto.py

--- functions/foto.py
+++ functions/foto.py
@@ -8,4 +8,6 @@ values = sheet.get_all_values()
 def count_foto_mancanti():
   
   df = pd.DataFrame(values[1:], columns=values[0])
-  st.dataframe(df)
+  scattare = len(df[df["SCATTARE"] == True])
+  riscattare = len(df[df["RISCATTARE"] == True])
+  st.write("Da Scattare: " + scattare)
