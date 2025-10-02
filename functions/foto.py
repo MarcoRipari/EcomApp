d69d72c264510fa32291bfac64edecfcbfa9703f
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-02 07:28:13 +0200

    Update foto.py

--- functions/foto.py
+++ functions/foto.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from .gsheet import get_sheet
 
-sheet = get_sheet(st.secrets("FOTO_GSHEET_ID"), "LISTA")
+sheet = get_sheet(st.secrets['FOTO_GSHEET_ID'], "LISTA")
 
 def count_foto_mancanti():
   df = pd.DataFrame(sheet.get_all_values())
