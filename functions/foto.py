529932835a5dc4514987ca71c81b52d612ae727a
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-02 10:57:00 +0200

    Update foto.py

--- functions/foto.py
+++ functions/foto.py
@@ -63,6 +63,7 @@ def mostra_riscattare(sku_input):
         value=st.session_state[key],
         key=key
       )
+      st.write("ok")
       
       # Controllo se lo stato è cambiato
       if ristampa_checkbox != st.session_state[key]:
