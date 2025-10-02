2c2a689928b5ff5bbd983e6328b65b372f609e32
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-02 10:19:24 +0200

    Update foto.py

--- functions/foto.py
+++ functions/foto.py
@@ -40,6 +40,17 @@ def mostra_riscattare(sku_input):
     row = match.iloc[0]
     st.write(row)
     image_url = f"https://repository.falc.biz/fal001{row['SKU'].lower()}-1.jpg"
+    cols = st.columns([1, 3, 1])
+    with cols[0]:
+        st.image(image_url, width=100, caption=row["SKU"])
+    with cols[1]:
+        st.markdown(f"**{row['DESCRIZIONE']}**")
+        st.markdown(f"*Canale*: {row['CANALE']}  \n*Collezione*: {row['COLLEZIONE']}")
+    with cols[2]:
+        if row['SKU'] in selected_ristampe:
+            ristampa_checkbox = st.checkbox("🔁 Ristampa", value=True, key=f"ristampa_{row['SKU']}")
+        else:
+            ristampa_checkbox = st.checkbox("🔁 Ristampa", value=False, key=f"ristampa_{row['SKU']}")
 
 def aggiungi_da_riscattare(sku_input):
   lista_da_riscattare = df[df["RISCATTARE"] == True]
