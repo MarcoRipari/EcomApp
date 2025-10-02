4d3ac6ddbeddf476e69d0012f334407ee1da8354
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-02 07:33:11 +0200

    Update foto.py

--- functions/foto.py
+++ functions/foto.py
@@ -7,5 +7,5 @@ sheet = get_sheet(st.secrets['FOTO_GSHEET_ID'], "LISTA")
 values = sheet.get_all_values()
 def count_foto_mancanti():
   
-  df = pd.DataFrame(values[1:], columns=values[0])
+  df = pd.DataFrame(values[1:], columns=values[1])
   st.dataframe(df)
