4d9056038602ab4873b95ba3a4a3aa56492fce86
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-06 17:47:20 +0200

    Update foto.py

--- functions/foto.py
+++ functions/foto.py
@@ -7,24 +7,17 @@ from .gsheet import get_sheet
 from .utils import normalize_bool
 from utils import *
 
+sheet = get_sheet(st.secrets['FOTO_GSHEET_ID'], "LISTA")
+values = sheet.get_all_values()
+df = pd.DataFrame(values[1:], columns=values[0])
 
-def load_sheet():
-  sheet = get_sheet(st.secrets['FOTO_GSHEET_ID'], "LISTA")
-  values = sheet.get_all_values()
-  df = pd.DataFrame(values[1:], columns=values[0])
-  
-  # Normalizzo i booleani (True / False)
-  df["SCATTARE"] = normalize_bool(df["SCATTARE"])
-  df["CONSEGNATA"] = normalize_bool(df["CONSEGNATA"])
-  df["RISCATTARE"] = normalize_bool(df["RISCATTARE"])
-  df["DISP"] = normalize_bool(df["DISP"])
-  df["DISP 027"] = normalize_bool(df["DISP 027"])
-  df["DISP 028"] = normalize_bool(df["DISP 028"])
-  return sheet, df
-  
-sheet, df = load_sheet()
-st.write(sheet)
-
+# Normalizzo i booleani (True / False)
+df["SCATTARE"] = normalize_bool(df["SCATTARE"])
+df["CONSEGNATA"] = normalize_bool(df["CONSEGNATA"])
+df["RISCATTARE"] = normalize_bool(df["RISCATTARE"])
+df["DISP"] = normalize_bool(df["DISP"])
+df["DISP 027"] = normalize_bool(df["DISP 027"])
+df["DISP 028"] = normalize_bool(df["DISP 028"])
 
 
 def count_da_scattare(type="totale"):
@@ -73,9 +66,11 @@ def mostra_riscattare(sku_input):
       if test:
         val = ["True"]
         sheet.update(f"L{nriga}", [val])
+        df.loc[df["SKU"] == row['sku'], "RISCATTARE"] == "True"
       else:
         val = ["False"]
         sheet.update(f"L{nriga}", [val])
+        df.loc[df["SKU"] == row['sku'], "RISCATTARE"] == "False"
           
 
 def aggiungi_da_riscattare(sku_input):
