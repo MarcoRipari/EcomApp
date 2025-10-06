ca808a3241b1039e0de8bef72f2fa5d68e2dc6c7
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-06 17:25:31 +0200

    Update foto.py

--- functions/foto.py
+++ functions/foto.py
@@ -65,13 +65,15 @@ def mostra_riscattare(sku_input):
       if test:
         nriga = df.index[df["SKU"] == row['SKU']].tolist()
         st.write(f"Attivare riga {nriga}!")
-        val = [f"\'True"]
-        sheet.update(f"L{nriga}", val)
+        print(nriga, type(nriga))
+        val = [[f"\'True"]]
+        sheet.update(f"L{nriga + 1}", val)
       else:
         nriga = df.index[df["SKU"] == row['SKU']].tolist()
         st.write(f"Disattivare riga {nriga}!")
-        val = [f"\'False"]
-        sheet.update(f"L{nriga}", val)
+        print(nriga, type(nriga))
+        val = [[f"\'False"]]
+        sheet.update(f"L{nriga + 1}", val)
           
 
 def aggiungi_da_riscattare(sku_input):
