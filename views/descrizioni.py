ae520ed87b4e3d8ee8021c427e13b3331c86eaa0
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-07 16:29:09 +0200

    Update descrizioni.py

--- views/descrizioni.py
+++ views/descrizioni.py
@@ -6,7 +6,7 @@ def genera_descrizioni():
     uploaded = st.file_uploader("Carica un file CSV", type="csv")
     
     if uploaded:
-        df_input = read_csv_auto_encoding(uploaded)
+        df_input = read_csv(uploaded)
         st.session_state["df_input"] = df_input
          # ✅ Inizializza variabili di stato se non esistono
         if "col_weights" not in st.session_state:
