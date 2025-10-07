2dfec89b874cc227140f64879782f4bfd81f4331
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-07 16:28:11 +0200

    Create read_csv.py

---
+++ utils/read_csv.py
@@ -0,0 +1,9 @@
+def read_csv(uploaded_file, separatore=None):
+    raw_data = uploaded_file.read()
+    result = chardet.detect(raw_data)
+    encoding = result['encoding'] or 'utf-8'
+    uploaded_file.seek(0)  # Rewind after read
+    if separatore:
+        return pd.read_csv(uploaded_file, sep=separatore, encoding=encoding, dtype=str)
+    else:
+        return pd.read_csv(uploaded_file, encoding=encoding, dtype=str)
