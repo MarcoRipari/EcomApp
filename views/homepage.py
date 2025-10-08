588aba42af7197baef0ea84781ad930e389dc5b3
MarcoRipari <marco.ripari.falc@gmail.com>
2025-10-08 07:52:29 +0200

    Update homepage.py

--- views/homepage.py
+++ views/homepage.py
@@ -8,6 +8,6 @@ load_functions_from("functions", globals())
 
 def homepage():
     st.title("Homepage")
-    
+    st.write("ok")
     sheet_id = st.text_input("Inserisci Sheet ID")
     tab = st.text_input("Inserisci Tab")
