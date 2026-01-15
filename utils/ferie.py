4a597ed1de8bade28be93b439173ec80ce4cfbb6
MarcoRipari <marco.ripari.falc@gmail.com>
2026-01-15 10:25:25 +0100

    Update ferie.py

--- utils/ferie.py
+++ utils/ferie.py
@@ -7,7 +7,18 @@ load_functions_from("functions", globals())
 
 ferie_sheet_id = st.secrets["FERIE_GSHEET_ID"]
 
+def calcola_giorni_lavorativi(inizio, fine):
+  giorni_lavorativi = 0
+  giorno_corrente = inizio
+  while giorno_corrente <= fine:
+    # 0 = Lunedì, 1 = Martedì, ..., 4 = Venerdì, 5 = Sabato, 6 = Domenica
+    if giorno_corrente.weekday() < 5:
+      giorni_lavorativi += 1
+    giorno_corrente += timedelta(days=1)
+  return giorni_lavorativi
+
 def add_ferie(riga):
+  st.write(riga[0])
   sheet = get_sheet(ferie_sheet_id,"FERIE")
   try:
     sheet.append_row(riga)
