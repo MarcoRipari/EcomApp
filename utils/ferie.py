12c036360f14da65e8f5883903e9a2fefb97bb77
MarcoRipari <marco.ripari.falc@gmail.com>
2026-01-15 09:55:01 +0100

    Update ferie.py

--- utils/ferie.py
+++ utils/ferie.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import gspread
 
-def aggiungi_ferie():
-  
+def aggiungi_ferie(sheet, riga):
+  sheet = connect_to_sheet()
+  nuova_riga = [nome, str(data_inizio), str(data_fine), tipo]
+  sheet.append_row(nuova_riga)
