3f9f4ecf24a69a9dab4725319e28e5569b2f7111
MarcoRipari <marco.ripari.falc@gmail.com>
2026-01-15 07:29:19 +0100

    Fix import statement for read_csv module

--- utils/process_csv_and_update.py
+++ utils/process_csv_and_update.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from .read_csv import *
+from read_csv import *
 
 def process_csv_and_update(sheet, uploaded_file, batch_size=100):
     st.text("1️⃣ Leggo CSV...")
