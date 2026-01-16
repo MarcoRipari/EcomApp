63556605fd5e9df02a3962c65ae5fdc6e869fd22
MarcoRipari <marco.ripari.falc@gmail.com>
2026-01-16 17:05:41 +0100

    Update ferie.py

--- views/ferie.py
+++ views/ferie.py
@@ -110,7 +110,7 @@ def ferie():
           giorni_residui = FERIE_TOTALI_ANNUE
   
       # Calcolo logica visuale
-      percentuale = min(giorni_goduti / dipendenti[dipendenti['NOME'] == nome_dipendente]['TOTALE'], 1.0)
+      percentuale = min(giorni_goduti / dati_report['TOTALE'], 1.0)
       colore_testo = "red" if giorni_residui < 5 else "#31333F"
       
       with cols[i % 3]:
