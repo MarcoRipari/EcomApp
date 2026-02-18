efa44fefa2f4f98d5c7324e7f9338651be643b3a
MarcoRipari <marco.ripari.falc@gmail.com>
2026-02-18 07:45:51 +0100

    Update foto.py

--- functions/foto.py
+++ functions/foto.py
@@ -32,7 +32,7 @@ def count_da_scattare(type="totale"):
   scattare = len(df[df["SCATTARE"] == True])
   riscattare = len(df[df["RISCATTARE"] == True])
   consegnate = len(df[(df["CONSEGNATA"] == True) & (df["SCATTARE"] == True)])
-  disponibili = len(df[(df["SCATTARE"] == True) & (df["CONSEGNATA"] == False) & (df["DISP"] == True)])
+  disponibili = len(df[(df["SCATTARE"] == True) & (df["CONSEGNATA"] == False) & ((df["DISP"] == True) || (df["DISP 027"] == True) || (df["DISP 012"] == True))])
   if type == "mancanti":
     return (scattare + riscattare) - consegnate
   elif type == "riscattare":
