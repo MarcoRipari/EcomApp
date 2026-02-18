40be8a3c6a1ca710f8783e589a52ff7b4cd3c2e1
MarcoRipari <marco.ripari.falc@gmail.com>
2026-02-18 07:31:21 +0100

    Update foto.py

--- views/foto.py
+++ views/foto.py
@@ -166,7 +166,7 @@ def foto_aggiungi_prelevate():
   
           if skus_to_append_clean:
               # Aggiungi apostrofo solo al momento dell'append per forzare formato testo
-              rows_to_append = [[f"'{sku}", f"{oggi}", f"=IMAGE(SOSTITUISCI(SETTINGS!$B$4;"*SKU*";MINUSC($A{(sheet_len+1)+i})))", f"=SE(VAL.NON.DISP(CONFRONTA(INDIRETTO(\"LISTA!$D\"&CONFRONTA($A{(sheet_len+1)+i};LISTA!A:A;0));SPLIT(SETTINGS(\"brandMatias\");\",\");0));SE(VAL.NON.DISP(CONFRONTA(INDIRETTO(\"LISTA!$D\"&CONFRONTA($A{(sheet_len+1)+i};LISTA!A:A;0));SPLIT(SETTINGS(\"brandMatteo\");\",\");0));\"\";\"MATTEO\");\"MATIAS\")", "ECOM"] for i, sku in enumerate(skus_to_append_clean)]
+              rows_to_append = [[f"'{sku}", f"{oggi}", f"=IMAGE(SOSTITUISCI(SETTINGS!$B$4;\"*SKU*\";MINUSC($A{(sheet_len+1)+i})))", f"=SE(VAL.NON.DISP(CONFRONTA(INDIRETTO(\"LISTA!$D\"&CONFRONTA($A{(sheet_len+1)+i};LISTA!A:A;0));SPLIT(SETTINGS(\"brandMatias\");\",\");0));SE(VAL.NON.DISP(CONFRONTA(INDIRETTO(\"LISTA!$D\"&CONFRONTA($A{(sheet_len+1)+i};LISTA!A:A;0));SPLIT(SETTINGS(\"brandMatteo\");\",\");0));\"\";\"MATTEO\");\"MATIAS\")", "ECOM"] for i, sku in enumerate(skus_to_append_clean)]
               
               
               # Append a partire dall'ultima riga disponibile
