d76a7243fccb2b4e4228459a104a07d1dcca51d7
MarcoRipari <marco.ripari.falc@gmail.com>
2026-02-23 08:04:39 +0100

    Update giacenze.py

--- views/giacenze.py
+++ views/giacenze.py
@@ -21,7 +21,7 @@ sheets_to_import = ['1MFwBu5qcXwD0Hti1Su9KTxl3Z9OLGtQtp1d3HJNEiY4', # FOTO
                    ]
 
 def giacenze_importa():
-    st.header("Importa giacenze2")
+    st.header("Importa giacenze")
 
     options = ["Manuale", "UBIC", "PIM"]
     
