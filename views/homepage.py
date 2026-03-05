78b9c3819bcfb708a1d7e21785858424067cf2ee
MarcoRipari <marco.ripari.falc@gmail.com>
2026-03-05 07:43:00 +0100

    Update homepage.py

--- views/homepage.py
+++ views/homepage.py
@@ -7,7 +7,7 @@ from utils import *
 load_functions_from("functions", globals())
 
 def homepage():
-    st.title("Homepage3")
+    st.title("Homepage")
 
     if st.sidebar.button("Svuota Cache"):
         st.cache_data.clear()
