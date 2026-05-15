a30a61e96a14c7e77835d4e92ded4f1cb0b3c8d7
MarcoRipari <marco.ripari.falc@gmail.com>
2026-05-15 17:06:13 +0200

    Update descrizioni.py

--- views/descrizioni.py
+++ views/descrizioni.py
@@ -230,7 +230,8 @@ def genera_descrizioni():
                     with st.spinner("✍️ Costruisco i prompt..."):
                         for _, row in df_input_to_generate.iterrows():
                             simili = retrieve_similar(row, index_df, index, k=k_simili, col_weights=st.session_state.col_weights) if k_simili > 0 else pd.DataFrame([])
-                            caption = get_blip_caption(row.get("Image 1", "")) if use_image and row.get("Image 1", "") else None
+                            #caption = get_blip_caption(row.get("Image 1", "")) if use_image and row.get("Image 1", "") else None
+                            caption = None
                             #prompt = build_unified_prompt(row, st.session_state.col_display_names, selected_langs, selected_tones, desc_lunga_length, desc_breve_length, image_caption=caption, simili=simili)
                             prompt = build_unified_prompt(
                                 row=row, 
