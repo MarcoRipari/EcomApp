71d2721321b7e1368b3d6305b8f1878926f24293
MarcoRipari <marco.ripari.falc@gmail.com>
2026-05-15 16:41:13 +0200

    Update descrizioni.py

--- views/descrizioni.py
+++ views/descrizioni.py
@@ -37,6 +37,7 @@ def genera_descrizioni():
 
     # 📊 Anteprima dati
     if "df_input" in st.session_state:
+        df_input[0]
         df_input = st.session_state.df_input
         st.subheader("🧾 Anteprima CSV")
         st.dataframe(df_input.head())
