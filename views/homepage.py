5a2c3444fed3697334f37c61fcb74345b1d2f10a
MarcoRipari <marco.ripari.falc@gmail.com>
2026-05-18 15:39:43 +0200

    Update homepage.py

--- views/homepage.py
+++ views/homepage.py
@@ -9,14 +9,14 @@ load_functions_from("functions", globals())
 def homepage():
     st.title("Homepage")
 
-    if st.sidebar.button("Svuota Cache"):
+    if st.button("Svuota Cache"):
         st.cache_data.clear()
         st.cache_resource.clear()
         st.success("Cache svuotata con successo!")
         st.rerun()
 
 
-    if st.sidebar.button("Svuota Memoria"):
+    if st.button("Svuota Memoria"):
         for key in st.session_state.keys():
             del st.session_state[key]
         st.success("Memoria svuotata con successo!")
