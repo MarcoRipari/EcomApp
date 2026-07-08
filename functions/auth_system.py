6904da9b394aad79029fd8a3c3828c483019b2cb
MarcoRipari <marco.ripari.falc@gmail.com>
2026-07-08 17:15:28 +0200

    Update auth_system.py

--- functions/auth_system.py
+++ functions/auth_system.py
@@ -12,7 +12,7 @@ def login(username: str, password: str) -> bool:
     output2 = 2
     try:
         # 1. Recupera il profilo dallo username
-        res_profile = supabase.table("profiles").select("*").eq("username", username).maybe().execute()
+        res_profile = supabase.table("profiles").select("*").eq("username", username).limit(1).execute()
         if not res_profile.data:
             st.error("❌ Username non trovato")
             return False
@@ -67,7 +67,7 @@ def login_password(email: str, password: str) -> bool:
         if res.user is not None:
            
             # Recupera il profilo dell'utente usando user_id
-            profile = supabase.table("profiles").select("*").eq("user_id", res.user.id).maybe().execute()
+            profile = supabase.table("profiles").select("*").eq("user_id", res.user.id).limit(1).execute()
             
             if profile.data is None:
                 st.error("❌ Profilo utente non trovato")
