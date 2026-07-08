a98d2860563d276f755593cd50aeb4c982de231a
MarcoRipari <marco.ripari.falc@gmail.com>
2026-07-08 16:53:12 +0200

    Change maybeSingle to single in auth_system.py

--- functions/auth_system.py
+++ functions/auth_system.py
@@ -10,7 +10,7 @@ supabase_admin = create_client(supabase_url, service_role_key)
 def login(username: str, password: str) -> bool:
     try:
         # 1. Recupera il profilo dallo username
-        res_profile = supabase.table("profiles").select("*").eq("username", username).maybeSingle().execute()
+        res_profile = supabase.table("profiles").select("*").eq("username", username).single().execute()
         if not res_profile.data:
             st.error("❌ Username non trovato")
             return False
@@ -30,7 +30,7 @@ def login(username: str, password: str) -> bool:
             "email": email,
             "password": password
         })
-
+        st.write(res)
         if res.user:
             # 4. Salva in session_state
             st.session_state.user = {
@@ -60,7 +60,7 @@ def login_password(email: str, password: str) -> bool:
         if res.user is not None:
            
             # Recupera il profilo dell'utente usando user_id
-            profile = supabase.table("profiles").select("*").eq("user_id", res.user.id).maybeSingle().execute()
+            profile = supabase.table("profiles").select("*").eq("user_id", res.user.id).single().execute()
             
             if profile.data is None:
                 st.error("❌ Profilo utente non trovato")
